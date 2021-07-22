670b29b73ff29a8bc24affeeb340437e522f230b
hoangtrung <hoangtrunga1k55@gmail.com>
2021-07-22 09:35:10 +0700

    sua code review login function

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use \Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['middleware' => ['localization', 'checkUser']], function () {
+    Route::post('login', [LoginController::class, 'handleLogin'])->name('auth.login')->withoutMiddleware('checkUser');
+
+    Route::get('logOut', [LoginController::class, 'logOut'])->name('auth.logout');
+
+    Route::get('login', [LoginController::class, 'loginForm'])->name('auth.loginForm');
+
+    Route::resource('tasks', TaskController::class)->only([
+        'index', 'store', 'destroy'
+    ]);
+
+    Route::get('change-language/{language}', [\App\Http\Controllers\LanguageController::class, 'changeLanguage'])->name('change-language');
+}
+);
